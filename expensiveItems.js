cb28b1c1adaadc2583452d979d6cb01fdcf54540
Mangaliso-Maduna <mangimaduna.mangaliso@gmail.com>
2022-07-05 00:45:56 +0200

    added descriptions to the widgets

--- expensiveItems.js
+++ expensiveItems.js
@@ -1,21 +1,15 @@
-function findItemsOver20(itemList){
-  let results=[]
-  for(let i=0;i<itemList.length;i++){
-    let list=itemList[i];
-    let quantity=list.qty
-    if( quantity> 20)
-      results.push(list)
+function totalPhoneBill(items){
+  var bill=items.split(",")
+  var sms=0;
+  var calls=0;
+  var totalCallsSms=0;
+  for(var i=0; i<bill.length;i++){
+    if(bill[i].trim()=="sms"){
+      sms=sms+0.65;
+    }else if(bill[i].trim()=="call"){
+      calls=calls+2.75;
+    }
   }
-  return results
-}
-
-function findItemsOver(itemList,x){
-  let results=[]
-  for(let i=0;i<itemList.length;i++){
-    let list=itemList[i];
-    let quantity=list.qty
-    if( quantity> x)
-      results.push(list)
-  }
-  return results
+  totalCallsSms=sms+calls;
+  return "R"+totalCallsSms.toFixed(2);
 }
